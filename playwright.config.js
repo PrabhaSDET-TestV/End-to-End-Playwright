1b72ea1543f28261d5dce864c241a2ee389231ed
PrabhaSDET-TestV <prabhakaranr@testvagrant.com>
2025-01-31 11:53:31 +0530

    Commit

--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,24 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
 
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+
+  expect: {
+    timeout: 5000
+  },
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-   
+   browserName: "chromium",
+   headless: false
   },
+
 });
 
--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -11,10 +11,78 @@ test("FirstScript", async({browser}) => {
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
 
 });
 test("FirstScript Page", async({page}) => {
     //Automatically done by playwright -> Instance of the new browser created and new page/tab also created
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+
+    const userName = page.locator(`#username`);
+    const pwd = page.locator(`[type='password']`);
+    const signIn = page.locator(`#signInBtn`);
+
+    await userName.fill(`rahulshettyacademy`);
+    await pwd.fill(`learn`);
+    await signIn.click();
+
+    console.log(await page.locator(`[style*='block']`).textContent());
+
+    await expect(page.locator(`[style*='block']`)).toContainText(`Incorrect`);
+
+    await pwd.fill(``);
+    await pwd.fill(`learning`);
+    await signIn.click();
+
+    const cardTitle = page.locator(`.card-body a`);
+    console.log(await cardTitle.first().textContent());
+    console.log(await cardTitle.nth(0).textContent());
+    
+    console.log(await cardTitle.allTextContents());
+});
+
+test("UI Controls", async({page}) => {
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    console.log(await page.title());
+
+    const dropdown = page.locator(`select.form-control`);
+    await dropdown.selectOption("consult");
+
+    await page.locator(`.radiotextsty`).nth(1).click();
+    await page.locator(`#okayBtn`).click();
+
+    console.log(await page.locator(`.radiotextsty`).nth(1).isChecked());
+    await expect(page.locator(`.radiotextsty`).nth(1)).toBeChecked();
+
+    await page.locator(`#terms`).click();
+    await expect(page.locator(`#terms`)).toBeChecked();
+    await page.locator(`#terms`).uncheck();
+    expect(await page.locator(`#terms`).isChecked()).toBeFalsy();
+
+    const documentLink = page.locator(`[href*='documents-request']`);
+    await expect(documentLink).toHaveAttribute("class","blinkingText");
+});
+
+test("Child page handle", async({browser}) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator(`[href*='documents-request']`);
+
+    const [ newPage ] = await Promise.all(
+        [
+            context.waitForEvent('page'),
+            documentLink.click()
+        ]
+    )
+
+    const text = await newPage.locator(`.red`).textContent();
+    console.log(text);
+    const arrayText = text.split("@");
+    const domain = arrayText[1].split(" ")[0].split(".")[0];
+    console.log(domain);
+
 
 });
